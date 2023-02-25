e6b9730964e971d0f27d7b99b853924d8c3973ae
Samet Çiftçi <temasictfic@gmail.com>
2023-02-25 04:01:35 +0300

    token

---
+++ parse/token.c
@@ -0,0 +1,85 @@
+
+#include "minishell.h"
+
+char	**create_argv(char *cmd, t_env *env)
+{
+	char	**argv;
+	t_token	**tokens;
+
+	tokens = create_tokens(cmd);
+	if (tokens == NULL || *tokens == NULL)
+	{
+		free_tokens(tokens);
+		return (NULL);
+	}
+	env_expand(env->local, tokens);
+	wildcard_expand(&tokens);
+	if (tokens[0]->value == NULL)
+	{
+		free_tokens(tokens);
+		return (NULL);
+	}
+	argv = token_to_argv(tokens);
+	free_tokens(tokens);
+	return (argv);
+}
+
+
+static char	*token_join(t_token *token)
+{
+	int		len;
+	char	*res;
+	int		i;
+	int		j;
+
+	len = 0;
+	i = -1;
+	while (token[++i].value)
+	{
+		j = -1;
+		while (token[i].value[++j])
+			len++;
+	}
+	res = malloc((len + 1) * sizeof(char));
+	res[len] = '\0';
+	len = 0;
+	i = -1;
+	while (token[++i].value)
+	{
+		j = -1;
+		while (token[i].value[++j])
+			res[len++] = token[i].value[j];
+	}
+	return (res);
+}
+
+char	**token_to_argv(t_token **tokens)
+{
+	char	**argv;
+	int		len;
+
+	len = 0;
+	while (tokens[len++])
+		;
+	argv = malloc(len * sizeof(char *));
+	if (argv == NULL)
+		return (NULL);
+	argv[--len] = NULL;
+	while (len--)
+		argv[len] = token_join(tokens[len]);
+	return (argv);
+}
+
+int	exec_tokens(char *cmd, t_env *env)
+{
+	char	**argv;
+	int		ret;
+
+	ret = 1;
+	argv = create_argv(cmd, env);
+	if (argv == NULL)
+		return (1);
+	exec_in_path(argv[0], argv, env->exported);
+	free_argv(argv);
+	return (ret);
+}
